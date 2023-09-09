b08bb1c1af5e486396e1d1ce3e3b38b9ea43d5c2
elvinlai1 <61441620+elvinlai1@users.noreply.github.com>
2023-09-08 17:32:29 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <div className='container mx-auto'>
+        <div className='flex flex-col justify-center items-center h-screen'>
+            <h1 className='text-4xl text-center font-bold'>FoodMe</h1>
+            <p>FoodMe in progress...</p>
+          </div>
+      </div>
+
+    </>
+  )
+}
+
+export default App
